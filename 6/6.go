64d5ae02e0f5ce714deddd4c494a24a15c9e1bb2
Radim Keske <radim.keske@tietoevry.com>
2024-12-13 13:35:31 +0100

    6. part 2

--- 6/6.go
+++ 6/6.go
@@ -7,67 +7,152 @@ import (
 	"strings"
 )
 
-func Load(path string) []string {
+type GuardPos struct {
+	X int
+	Y int
+}
+
+var rotate_counter int = 0
+
+func Load(path string) [][]string {
 	data, _ := os.ReadFile(path)
-	return strings.Split(strings.TrimSpace(string(data)), "\n")
-}
+	rows := strings.Split(strings.TrimSpace(string(data)), "\n")
+	matrix := make([][]string, len(rows))
 
-func Rotate(lines []string) []string {
-	result := make([]string, len(lines))
-	for _, v := range lines {
-		for j, char := range v {
-			result[j] += string(char)
+	for i, row := range rows {
+		matrix[i] = make([]string, len(row))
+		for j, col := range row {
+			matrix[i][j] = string(col)
 		}
 	}
-
-	slices.Reverse(result)
-	return result
-}
-
-func FindGuard(matrix []string) int {
-	for i, row := range matrix {
-		if strings.Contains(row, "^") {
-			return i
-		}
-	}
-	return -1
-}
-
-func MoveUp(matrix []string) []string {
-	guard_row := FindGuard(matrix)
-	guard_col := strings.Index(matrix[guard_row], "^")
-
-	if guard_row == 0 {
-		str := []rune(matrix[guard_row])
-		str[guard_col] = 'X'
-		matrix[guard_row] = string(str)
-		return matrix
-	}
-
-	if matrix[guard_row-1][guard_col] == '#' {
-		return Rotate(matrix)
-	}
-
-	str := []rune(matrix[guard_row-1])
-	str[guard_col] = '^'
-	matrix[guard_row-1] = string(str)
-	matrix[guard_row] = strings.Replace(matrix[guard_row], "^", "X", 1)
 	return matrix
 }
 
-func VisitedPosCounter(matrix []string) (result int) {
+func Rotate(matrix [][]string) {
+	tmp := make([][]string, len(matrix))
+
+	for i, row := range matrix {
+		tmp[i] = make([]string, len(row))
+		for j, _ := range row {
+			tmp[i][j] = matrix[j][i]
+		}
+	}
+
+	for i, row := range tmp {
+		for j, _ := range row {
+			matrix[i][j] = tmp[i][j]
+		}
+	}
+	slices.Reverse(matrix)
+	rotate_counter++
+}
+
+func FindGuard(matrix [][]string) GuardPos {
+	for i, row := range matrix {
+		for j, _ := range row {
+			if matrix[i][j] == "^" {
+				return GuardPos{X: i, Y: j}
+			}
+		}
+	}
+	return GuardPos{X: -1, Y: -1}
+}
+
+func MoveUp(matrix [][]string) {
+	guard := FindGuard(matrix)
+
+	if guard.X == 0 {
+		matrix[guard.X][guard.Y] = "X"
+		return
+	}
+
+	if matrix[guard.X-1][guard.Y] == "#" {
+		Rotate(matrix)
+		return
+	}
+
+	matrix[guard.X][guard.Y] = "X"
+	matrix[guard.X-1][guard.Y] = "^"
+
+	return
+}
+
+func VisitedPosCounter(matrix [][]string) (result int) {
 	for _, row := range matrix {
-		result += strings.Count(row, "X")
+		for _, col := range row {
+			if col == "X" {
+				result += 1
+			}
+		}
 	}
 	return
 }
 
-func main() {
-	matrix := Load("input")
+func GuardRouteSteps(matrix [][]string) int {
+	steps := 0
+	for FindGuard(matrix).X >= 0 {
+		steps++
+		MoveUp(matrix)
 
-	for FindGuard(matrix) >= 0 {
-		matrix = MoveUp(matrix)
+		if steps > 10000 {
+			return -1
+		}
 	}
 
-	fmt.Printf("part 1 result is: %v", VisitedPosCounter(matrix))
+	return VisitedPosCounter(matrix)
+}
+
+func GetVisited(matrix [][]string) []GuardPos {
+	// reset to original orientation
+	for rotate_counter%4 != 0 {
+		Rotate(matrix)
+	}
+
+	positions := []GuardPos{}
+
+	for i, row := range matrix {
+		for j, _ := range row {
+			if matrix[i][j] == "X" {
+				positions = append(positions, GuardPos{X: i, Y: j})
+			}
+		}
+	}
+	return positions
+
+}
+
+func DisplayMatrix(matrix [][]string) {
+	for _, row := range matrix {
+		fmt.Println(strings.Join(row, ""))
+	}
+	fmt.Println()
+}
+
+func main() {
+	filename := "input"
+	matrix := Load(filename)
+	start_pos := FindGuard(matrix)
+	result1 := GuardRouteSteps(matrix)
+	path := GetVisited(matrix)
+
+	fmt.Printf("part1 result: %v, visited positions: %v, guard start_pos: %v\n", result1, len(path), start_pos)
+	DisplayMatrix(matrix)
+
+	var loops int
+	for _, pos := range path {
+		if pos == start_pos {
+			continue
+		}
+		matrix = Load(filename)
+		matrix[pos.X][pos.Y] = "#"
+
+		if GuardRouteSteps(matrix) == -1 {
+			loops++
+			fmt.Printf("%v loop detected with obstacle on\n", pos)
+		}
+
+	}
+
+	fmt.Printf("part2 result: %v", loops)
+	// took hint from reddit that obstacle may only appear on Guard usual route
 }
--- 6/6_test.go
+++ 6/6_test.go
@@ -6,11 +6,19 @@ import (
 )
 
 func TestRotate(t *testing.T) {
-	start := []string{"123", "456", "789"}
-	want := []string{"123", "456", "789"}
+	start := [][]string{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
+	}
+	want := [][]string{
+		{"1", "2", "3"},
+		{"4", "5", "6"},
+		{"7", "8", "9"},
+	}
 
 	for range 4 {
-		start = Rotate(start)
+		Rotate(start)
 	}
 
 	if !reflect.DeepEqual(start, want) {
@@ -19,37 +27,78 @@ func TestRotate(t *testing.T) {
 }
 
 func TestFindGuard(t *testing.T) {
-	got := FindGuard([]string{"...", "...", "..^"})
-	want := 2
+	matrix := [][]string{
+		{".", ".", "."},
+		{".", ".", "."},
+		{".", ".", "^"},
+	}
+	got := FindGuard(matrix)
+	want := GuardPos{X: 2, Y: 2}
 
 	if got != want {
 		t.Errorf("wanted %v but got %v", want, got)
 	}
+
+	matrix[2][2] = "."
+	got = FindGuard(matrix)
+	want = GuardPos{X: -1, Y: -1}
+
+	if got != want {
+		t.Errorf("wanted %v but got %v", want, got)
+	}
+
 }
 
 func TestMoveUp(t *testing.T) {
-	got := MoveUp([]string{"...", "...", ".^."})
-	want := []string{"...", ".^.", ".X."}
+	matrix := [][]string{
+		{".", ".", "."},
+		{".", ".", "."},
+		{".", "^", "."},
+	}
+	MoveUp(matrix)
+	want := [][]string{
+		{".", ".", "."},
+		{".", "^", "."},
+		{".", "X", "."},
+	}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("wanted %q but got %q", want, got)
+	if !reflect.DeepEqual(matrix, want) {
+		t.Errorf("wanted %q but got %q", want, matrix)
 	}
 }
 
 func TestMoveUpWithObstacle(t *testing.T) {
-	got := MoveUp([]string{"...", ".#.", ".^."})
-	want := Rotate([]string{"...", ".#.", ".^."})
+	matrix := [][]string{
+		{".", ".", "."},
+		{".", "#", "."},
+		{".", "^", "."},
+	}
+	MoveUp(matrix)
+	want := [][]string{
+		{".", ".", "."},
+		{".", "#", "^"},
+		{".", ".", "."},
+	}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("wanted %q but got %q", want, got)
+	if !reflect.DeepEqual(matrix, want) {
+		t.Errorf("wanted %q but got %q", want, matrix)
 	}
 }
 
 func TestMoveUpFinish(t *testing.T) {
-	got := MoveUp([]string{"^..", "...", "..."})
-	want := []string{"X..", "...", "..."}
+	matrix := [][]string{
+		{"^", ".", "."},
+		{".", ".", "."},
+		{".", ".", "."},
+	}
+	MoveUp(matrix)
+	want := [][]string{
+		{"X", ".", "."},
+		{".", ".", "."},
+		{".", ".", "."},
+	}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("wanted %q but got %q", want, got)
+	if !reflect.DeepEqual(matrix, want) {
+		t.Errorf("wanted %q but got %q", want, matrix)
 	}
 }
